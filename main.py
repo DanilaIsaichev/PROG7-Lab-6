f8c1476b4a016d3472e18e6e52837ff7031cc0e6
Danila Isaichev <danila.isaichev@mail.ru>
2022-10-25 22:16:48 +0300

    Laptop commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
@@ -21,6 +21,18 @@ from natasha import (
 )
 from typing import Dict
 from collections import Counter
+
+
+def get_news_text(news_tags: list) -> str:
+
+  news_text = ""
+
+  for news_tag in news_tags:
+      tag_text = " ".join(news_tag.text.split())
+      if news_tag != "\n" and tag_text != "": #and " ".join(news_tag.text.split()) != "" and news_tag.text.strip() != u'\xa0':
+        news_text += tag_text + " "
+
+  return news_text
 
 
 def extract_ners(spans: list) -> Dict[str, list]:
@@ -63,12 +75,19 @@ def extract_ners(spans: list) -> Dict[str, list]:
         elif span.type == ORG:
             span.normalize(morph_vocab)
             
-            # Проверка не является ли название организации аббревиатурой
-            if span.tokens[0].text.isupper() == True:
-                # Добавление ненормализированной строки
-                # РГПУ им. А.И. Герцена - ненормализированная строка
-                # РГПУ им. А.И. Герцен - нормализированная строка
-                organizations.append(span.text)
+            # Проверка не содержит ли название организации "им"
+            if "им " in span.text:
+              organization_name = span.normal[:span.normal.find("имя")] + span.text[span.text.find("им "):]
+              organizations.append(organization_name)
+
+            elif "им." in span.text:
+              organization_name = span.normal[:span.normal.find("имя")] + span.text[span.text.find("им."):]
+              organizations.append(organization_name)
+
+            elif "имени" in span.text:
+              organization_name = span.normal[:span.normal.find("имя")] + span.text[span.text.find("имени"):]
+              organizations.append(organization_name)
+
             else:
                 # Добавление нормализированной строки
                 organizations.append(span.normal)
@@ -91,89 +110,155 @@ ner_tagger = NewsNERTagger(emb)
 
 names_extractor = NamesExtractor(morph_vocab)
 
+base_url = 'https://www.herzen.spb.ru'
 
-text = """Уже три дня РГПУ им. А. И. Герцена совместно со школами Санкт-Петербурга реализует Тематическую смену для обучающихся классов психолого-педагогической направленности.
-Первый день прошел на базе РГПУ им. А. И. Герцена, второй день- на базе ГБОУ СОШ №27 с углубленным изучением литературы, истории и иностранных языков Василеостровского района им. И.А.Бунина, третий, завершающий день 21 октября, прошёл в ГБОУ СОШ № 47 им. Д.С. Лихачева Петроградского района.
-Ученики психолого-педагогических классов и студенты-тьюторы совместно дорабатывали проекты, тренировались защищать работы в виде стендовых докладов. Из учеников, тьюторов и мастеров-преподавателей были сформированы экспертные группы, которые, посетив все мастерские и изучив проекты, задав уточняющие вопросы ребятам, выставили оценки и выбрали несколько лучших проектов.
-В рамках подведения итогов директор ГБОУ СОШ № 47 им. Д.С. Лихачева Петроградского района Обухова М.Ю. выступила с напутственным словом, в котором призвала учеников «чтить прошлое, творить настоящее, верить в будущее!», научный руководитель работы с классами психолого-педагогической направленности РГПУ им. А. И. Герцена Ирина Кондракова пожелала ребятам найти свое место в жизни и выбрать правильный путь, после чего обучающиеся презентовали лучшие проекты.
-Поздравляем с завершением Тематической смены и желаем ребятам успешной практики!"""
+news = []
 
-doc = Doc(text)
+url = base_url + '/main/news/'
+html = urlopen(url)
+bs = BeautifulSoup(BeautifulSoup(html, 'html.parser').prettify(), 'html.parser')
 
-doc.segment(segmenter)
-doc.tag_morph(morph_tagger)
-doc.tag_ner(ner_tagger)
+tags = bs.find_all('a', {'class': 'news_header_link'})
 
-ner_elements_lists = extract_ners(doc.spans)
+for tag in tags:
+  news_url = base_url + tag['href'].strip()
+  
+  print(news_url)
+  
+  news_html = urlopen(news_url)
+  
+  news_bs = BeautifulSoup(BeautifulSoup(news_html, 'html.parser').prettify(), 'html.parser')
+  
+  news_tags = news_bs.find('div', {'style': 'padding:0 5 5 5px'})
+  
+  news_text = get_news_text(news_tags.contents)
+  
+  doc = Doc(news_text)
 
-# Счётчик действующих лиц
-actors_amount = Counter(ner_elements_lists["actors"])
+  doc.segment(segmenter)
+  doc.tag_morph(morph_tagger)
+  doc.tag_ner(ner_tagger)
+  
+  ner_elements_lists = extract_ners(doc.spans)
 
-# Счётчик мест
-locations_amount = Counter(ner_elements_lists["locations"])
+  # Счётчик действующих лиц
+  news_actors = list(Counter(ner_elements_lists["actors"]))
 
-# Счётчик организаций
-organizations_amount = Counter(ner_elements_lists["organizations"])
+  # Счётчик мест
+  news_locations = list(Counter(ner_elements_lists["locations"]))
 
-# Список слов, из которых состоят actors, locations и organizations
-ners_words = []
+  # Счётчик организаций
+  news_organizations = list(Counter(ner_elements_lists["organizations"]))
 
-for keys in ner_elements_lists:
-    for ner_element in ner_elements_lists[keys]:
-        for word in ner_element.split(" "):
-            # Удаляем служебные символы
-            ners_words.append(word.lower().strip("""!()-[]{};?@#$%:'"\,./^&amp;*_"""))
-print(ners_words)
+  # Список слов, из которых состоят actors, locations и organizations
+  ners_words = []
 
-key_words = []
-key_words_tokens= []
+  for keys in ner_elements_lists:
+      for ner_element in ner_elements_lists[keys]:
+          for word in ner_element.split(" "):
+              # Удаляем служебные символы
+              ners_words.append(word.lower().strip("""!()-[]{};?@#$%:'"\,./^&amp;*_"""))
 
-for token in doc.tokens:
-    # Проверка токенов 
-    if token.pos not in ["PROPN", "PUNCT", "CCONJ", "ADP", "PART", "PRON", "NUM", "SYM"] and token.rel != 'iobj':
-        token.lemmatize(morph_vocab)
+  key_words = []
+  key_words_tokens= []
 
-        # Удаляем служебные символы
-        token.text = token.text.strip("""!()-[]{};?@#$%:'"\,./^&amp;*_""")
-        token.lemma = token.lemma.strip("""!()-[]{};?@#$%:'"\,./^&amp;*_""")
+  for token in doc.tokens:
+      # Проверка токенов 
+      if token.pos not in ["PROPN", "PUNCT", "CCONJ", "ADP", "PART", "PRON", "NUM", "SYM"] and token.rel != 'iobj':
+          token.lemmatize(morph_vocab)
 
-        # Проверяем, не встречается ли слово в actors, organizations и locations
-        if token.text not in ners_words and token.lemma not in ners_words:
-            key_words_tokens.append(token)
-            key_words.append(token.lemma)
+          # Удаляем служебные символы
+          token.text = token.text.strip("""!()-[]{};?@#$%:'"\,./^&amp;*_""")
+          token.lemma = token.lemma.strip("""!()-[]{};?@#$%:'"\,./^&amp;*_""")
 
-# Счётчик ключевых слов
-key_words_amount = Counter(key_words)
+          # Проверяем, не встречается ли слово в actors, organizations и locations
+          if token.text not in ners_words and token.lemma not in ners_words:
+              key_words_tokens.append(token)
+              key_words.append(token.lemma)
 
-# ТОП 10
-if len(actors_amount) < 10:
-    print(list(actors_amount.items()))
-else:
-    print(actors_amount.most_common(10))
+  # Счётчик ключевых слов
+  key_words_amount = Counter(key_words)
+
+  # ТОП 10
+  if len(news_actors) != 0:
+    if len(news_locations) != 0:
+      if len(news_organizations) != 0:
+        news.append(
+          {
+            "header": tag.get_text().strip(),
+            "link": base_url + tag['href'].strip(),
+            "text": news_text,
+            "actors": news_actors,
+            "locations": news_locations,
+            "organiztions": news_organizations,
+            "key_words": key_words
+          })
+      else:
+        news.append(
+          {
+            "header": tag.get_text().strip(),
+            "link": base_url + tag['href'].strip(),
+            "text": news_text,
+            "actors": news_actors,
+            "locations": news_locations,
+            "key_words": key_words
+          })
+    elif len(news_organizations) != 0:
+      news.append(
+        {
+          "header": tag.get_text().strip(),
+          "link": base_url + tag['href'].strip(),
+          "text": news_text,
+          "actors": news_actors,
+          "organiztions": news_organizations,
+          "key_words": key_words
+        }) 
+    else:
+      news.append(
+          {
+            "header": tag.get_text().strip(),
+            "link": base_url + tag['href'].strip(),
+            "text": news_text,
+            "actors": news_actors,
+            "key_words": key_words
+          })
+  elif len(news_locations) != 0:
+    if len(news_organizations) != 0:
+      news.append(
+        {
+          "header": tag.get_text().strip(),
+          "link": base_url + tag['href'].strip(),
+          "text": news_text,
+          "locations": news_locations,
+          "organiztions": news_organizations,
+          "key_words": key_words
+        })
+    else:
+      news.append(
+        {
+          "header": tag.get_text().strip(),
+          "link": base_url + tag['href'].strip(),
+          "text": news_text,
+          "locations": news_locations,
+          "key_words": key_words
+        })
+  else:
+    news.append(
+      {
+        "header": tag.get_text().strip(),
+        "link": base_url + tag['href'].strip(),
+        "text": news_text,
+        "organiztions": news_organizations,
+        "key_words": key_words
+      })
 
 
-if len(locations_amount) < 10:
-    print(list(locations_amount.items()))
-else:
-    print(locations_amount.most_common(10))
 
+  text_raw = r" ".join(key_word for key_word in key_words)
+  print(text_raw)
+  wordcloud = WordCloud().generate(text_raw)
 
-if len(organizations_amount) < 10:
-    print(list(organizations_amount.items()))
-else:
-    print(organizations_amount.most_common(10))
-
-
-if len(key_words_amount) < 10:
-    print(list(key_words_amount.items()))
-else:
-    print(key_words_amount.most_common(10))
-
-
-text_raw = " ".join(key_word for key_word in key_words_amount.elements())
-print(text_raw)
-wordcloud = WordCloud().generate(text_raw)
-
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.show()
+  plt.imshow(wordcloud, interpolation='bilinear')
+  plt.axis("off")
+  plt.show()
+  print(news)
